d256f781f1f938f4ac867521d4de0a0cd83849d6
EngineerDogIta <bellayari94@gmail.com>
2017-05-11 20:52:21 +0200

    Added Lezione_1

---
+++ Lezione_1/src/divisione.java
@@ -0,0 +1,18 @@
+
+public class divisione {
+	public static void main(String[] args) {
+		int D = 100;
+		int d = 7;
+		int Q, r;
+
+		Q = 0;
+		r = D;
+		while (r >= d) {
+			r = r - d;
+			Q = Q + 1;
+		}
+
+		System.out.println("Il quoziente è " + Q);
+		System.out.println("Il resto è " + r);
+	}
+}
