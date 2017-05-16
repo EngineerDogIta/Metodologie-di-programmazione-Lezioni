fdda3d61c805b42d0c3f68e1d0cc64afff54df42
EngineerDogIta <bellayari94@gmail.com>
2017-05-16 09:11:51 +0200

    Fixed lezione_2 not working
    Missing main method fixed

--- Lezione_2/src/Variabili.java
+++ Lezione_2/src/Variabili.java
@@ -1,14 +1,16 @@
 
 public class Variabili {
 	// gioca con le variabili
+	public static void main(String[] args) {
+		Var v1 = new Var(), v2 = new Var();
+		
+		// v1.valore = 3; NO!
+		// v2.valore = 4; NO!
+		
+		v1.assegna(3);
+		v2.assegna(4); //  se non assegno contiene il valore di default
+		
+		System.out.println(v1.leggi() + v2.leggi());
+	}
 	
-	Var v1 = new Var(), v2 = new Var();
-	
-	// v1.valore = 3; NO!
-	// v2.valore = 4; NO!
-	
-	v1.assegna(3);
-	v2.assegna(4); //  se non assegno contiene il valore di default
-	
-	System.out.println(v1.leggi() + v2.leggi());
 }
